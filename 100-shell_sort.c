70e826408a7d85e1f37e1734e70cf110149daf9f
nonsoflex <emmflexcsc400@gmail.com>
2023-11-21 02:15:52 -0800

    sorting_algorithms

---
+++ 100-shell_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+void insertion_sort(int *, long int, long int, size_t);
+
+/**
+* shell_sort - sorts using shell sort and knuths sequence
+*
+* @array: array to sort
+* @size: size of array
+*/
+void shell_sort(int *array, size_t size)
+{
+	long int n = 1;
+	long int i, k, temp;
+
+	if (size < 2)
+		return;
+
+	while (n < (long int)size / 3)
+		n = n * 3 + 1;
+
+	while (n > 0)
+	{
+		for (i = n; i < (long int) size; i++)
+		{
+			temp = array[i];
+			for (k = i; k >= n && array[k - n] > temp; k = k - n)
+				array[k] = array[k - n];
+			array[k] = temp;
+		}
+		print_array(array, size);
+		n = (n - 1) / 3;
+	}
+}
---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,62 @@
+#include "sort.h"
+
+void swap_list(listint_t *curr, listint_t *prev, listint_t **head);
+
+/**
+* cocktail_sort_list - Sorts a Doubly linked list with cocktail sort
+* @list: A Doubly linked list
+*/
+
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *cur;
+	listint_t *max = NULL;
+	listint_t *min = NULL;
+
+	if (!list || !(*list) || (*list)->next == NULL)
+		return;
+	cur = *list;
+	do {
+		while (cur->next)
+		{
+			if (cur->n > cur->next->n)
+				swap_list(cur->next, cur, list);
+			else
+				cur = cur->next;
+		}
+		max = cur;
+		while (cur->prev != min)
+		{
+			if (cur->n < cur->prev->n)
+				swap_list(cur, cur->prev, list);
+			else
+				cur = cur->prev;
+		}
+		min = cur;
+	} while (min != max);
+}
+
+/**
+* swap_list - swaps two members of a list
+*
+* @curr: current node
+* @prev: previous node
+* @head: head of list
+*/
+void swap_list(listint_t *curr, listint_t *prev, listint_t **head)
+{
+	listint_t *temp1 = curr->next;
+	listint_t *temp2 = prev->prev;
+
+	if (temp1 != NULL)
+		temp1->prev = prev;
+	if (temp2 != NULL)
+		temp2->next = curr;
+	curr->prev = temp2;
+	prev->next = temp1;
+	curr->next = prev;
+	prev->prev = curr;
+	if (*head == prev)
+		*head = curr;
+	print_list(*head);
+}
